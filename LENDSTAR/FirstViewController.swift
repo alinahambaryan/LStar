aace3642d059ba294676810703e0bc89246d4dcb
ALINA HAMBARYAN <alinah@alinas-macbook-pro.local>
2016-11-24 12:39:54 +0400

    Initial Commit

---
+++ LENDSTAR/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LENDSTAR
+//
+//  Created by ALINA HAMBARYAN on 11/24/16.
+//  Copyright © 2016 ALINA HAMBARYAN. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LENDSTAR/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LENDSTAR
+//
+//  Created by ALINA HAMBARYAN on 11/24/16.
+//  Copyright © 2016 ALINA HAMBARYAN. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
